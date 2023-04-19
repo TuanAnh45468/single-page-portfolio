d8269eeb90fb895be668e8b6f1b412017eb8992d
tuana <tuananh45468@gmail.com>
2023-04-19 17:22:43 +0700

    Initialize the project

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./starter-code/dist/**/*.{html,js}"],
+  theme: {
+    colors:{
+      "black": "hsl(0, 0%, 8%)",
+      "dark-green": "hsl(0, 0%, 14%)",
+      "green": "hsl(153, 71%, 59%)",
+      "grey": "hsl(0, 0%, 85%)",
+      "white": "hsl(0, 0%, 100%)"
+    },
+
+    extend: {
+      fontFamily: {
+        "space-grotesk": ['Space Grotesk', 'sans-serif']
+      },
+      fontWeight:{
+        "medium": 500,
+        "bold": 700,
+      }
+    },
+  },
+  plugins: [],
+}
+
