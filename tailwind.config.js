0d289b7a9561ccb894fd03e2e8c28a09173e6ae9
tuana <tuananh45468@gmail.com>
2023-04-21 14:27:26 +0700

    Complete here section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,27 @@
 module.exports = {
   content: ["./starter-code/dist/**/*.{html,js}"],
   theme: {
-    colors:{
-      "black": "hsl(0, 0%, 8%)",
+    colors: {
+      black: "hsl(0, 0%, 8%)",
       "dark-green": "hsl(0, 0%, 14%)",
-      "green": "hsl(153, 71%, 59%)",
-      "grey": "hsl(0, 0%, 85%)",
-      "white": "hsl(0, 0%, 100%)"
+      green: "hsl(153, 71%, 59%)",
+      grey: "hsl(0, 0%, 85%)",
+      white: "hsl(0, 0%, 100%)",
     },
 
     extend: {
       fontFamily: {
-        "space-grotesk": ['Space Grotesk', 'sans-serif']
+        "space-grotesk": ["Space Grotesk", "sans-serif"],
       },
-      fontWeight:{
-        "medium": 500,
-        "bold": 700,
-      }
+      fontWeight: {
+        medium: 500,
+        bold: 700,
+      },
+
+      textDecorationColor: {
+        green: "hsl(153, 71%, 59%)",
+      },
     },
   },
   plugins: [],
-}
-
+};
